d6db0fcc92e92839595e9d476b4eb342ac89da6d
kishant-cagrand <kishant@cagrand.com>
2025-12-31 17:14:11 +0800

    integrate

--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,22 @@ import adminDashboardRoutes from './routes/adminDashboard.js';
 dotenv.config();
 
 const app = express();
+const allowedOrigins = [
+  'http://localhost:3000', // local dev
+  'https://cg-ver-2.vercel.app' // your deployed frontend
+];
 
 // === MIDDLEWARE ===
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true); // allow non-browser requests like Postman
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
 }));
 
 app.use(express.json());
